96630ddf72ac4c76ba99211ad0e7f14851afdbbb
DMCzero <2410451458@qq.com>
2020-06-20 23:05:15 +0800

    Site updated: 2020-06-20 23:05:15

--- js/comment-issue-data.js
+++ js/comment-issue-data.js
@@ -1,8 +1,8 @@
 // 评论issues仓库 by.removeif https://removeif.github.io/
 var repoIssuesUrl = "https://api.github.com/repos/removeif/blog_comment/issues";
 // 对应仓库 clientId、clientSecret 关于这两个参数的安全问题，查看 https://removeif.github.io/2019/09/19/博客源码分享.html#1-热门推荐，最新评论：
-var clientId = "46a9f3481b46ea0129d8";
-var clientSecret = "79c7c9cb847e141757d7864453bcbf89f0655b24";
+var clientId = "241a19d4a30d490276c6";
+var clientSecret = "8c91c8526e1cd80f3c86e2d3f6a1777280f32293";
 var authorizationToken = 'Basic ' + btoa(clientId + ':' + clientSecret);
 // 写comment count值
 var reqCommentCountUrl = repoIssuesUrl + "?t=" + new Date().getTime() + "&labels=Gitalk,";
